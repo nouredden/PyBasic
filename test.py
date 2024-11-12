4d16e1859449c5d2337817fef483645dec637546
NOUR ALREZ <noureddena@architweb.com>
2024-11-12 11:11:32 +0800

    first and second lesson done

---
+++ test.py
@@ -0,0 +1,17 @@
+# this is a comment
+"""this is a 
+multiple line c
+omment but it i
+s not working , IDK why.!! """
+"""now 
+the multi line 
+comment 
+work
+because 
+we 
+have to write the double qoutations three times at the beggining and the end"""
+print("hello world")
+mylist = ["nour", "douaa", "zainab", "riyad"]
+print(mylist)
+mylist.append("tarek")
+print(mylist)
