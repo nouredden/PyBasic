9a5652e72cd01e32a59a8d9e77c6b547a011ec82
NOUREDDEN ALREZ <noureddenrz@outlook.com>
2024-11-13 11:55:23 +0800

    Practice 1.10 Solved

--- book01.py
+++ book01.py
@@ -16,3 +16,18 @@ print (x[0])
 print (x[1])
 y = int (x)
 print (y)
+print(chr(y))
+
+""" Practice 1.10
+Write a short program that computes the length of the
+hypotenuse of a right triangle given the two legs as pictured in Fig. 1.23 on
+p. 35. The program should use three variables, sideA, sideB, and sideC. The
+Pythagorean theorem states that the sum of the squares of the two legs of
+the triangle equals the square of the hypotenuse. Be sure to assign all three
+variables their correct values and print the length of sideC at the end of the
+program. HINT: Raising a value to the 1/2 power is the same thing as finding
+the square root. Try values 6 and 8 for sideA and sideB."""
+sideA = int(input("Enter the SideA length: "))
+sideB = int(input("Enter the SideB length: "))
+sideC = (sideA**2 + sideB**2)**0.5
+print(f"The length of Pythagorean (sideC) is : {sideC}")
